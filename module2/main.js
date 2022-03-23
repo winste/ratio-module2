fec28f4732e2ffa7ef78fc5f2cb7e671688ba9c4
Viktoriya <step.vikulya@mail.ru>
2022-03-23 21:23:15 +0700

    Переделана таблица на js

--- module2/main.js
+++ module2/main.js
@@ -1,1 +1,32 @@
-import './style.css'
+let tableArray;
+let size = 4;
+
+
+function createTable() {
+    let table = document.createElement('table');
+    table.className = 'table';
+    table.setAttribute('id', 'table');
+    document.body.prepend(table);
+}
+
+function createRows() {
+    tableArray = [];
+    var table = document.getElementById('table');
+    for (let i = 0; i < size; i++) {
+        let tr = document.createElement('tr');
+        tr.className = 'table__row';
+        let trArray = [];
+        for (let j = 0; j < size; j++) {
+            let td = document.createElement('td');
+            td.className = 'table__cell';
+            tr.appendChild(td);
+            trArray.push(td);
+        }
+        tableArray.push(trArray);
+        table.appendChild(tr);
+        console.log(tableArray);
+    }
+}
+
+createTable();
+createRows();
